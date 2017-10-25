665edae11eee2617989400ee427b258ef4b26931
Zack Strickland <zack.a.strickland@gmail.com>
2017-10-24 21:27:43 -0400

    Add edit/update to events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,4 +6,20 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(event_params)
+    redirect_to @event
+  end
+
+  private
+
+    def event_params
+      params.require(:event).permit(:name, :description, :location, :price, :starts_at)
+    end
 end
