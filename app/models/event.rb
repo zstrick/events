87b73bb67afc680592ee4390b13467e96d82365c
Zack Strickland <zack.a.strickland@gmail.com>
2017-11-01 15:55:18 -0400

    Add scopes to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,12 +14,13 @@ class Event < ApplicationRecord
     message: "must reference a GIF, JPG, or PNG image"
   }
 
+  scope :past, -> { where("starts_at < ?", Time.now).order(:starts_at) }
+  scope :upcoming, -> { where("starts_at >= ?", Time.now).order(:starts_at) }
+  scope :free, -> { upcoming.where(price: 0).order(:name) }
+  scope :recent, ->(max=3) { past.limit(max) }
+
   def free?
     price.blank? || price.zero?
-  end
-
-  def self.upcoming
-    where("starts_at >= ?", Time.now).order(:starts_at)
   end
 
   def spots_left
